8adaf6de0f1584353209ad799fb03d19b388ec4a
rahul.tandon <rahul.tandon@flipkart.com>
2020-12-12 03:28:15 +0530

    add spark job

--- topology/src/main/java/com/main/java/utils/Constants.java
+++ topology/src/main/java/com/main/java/utils/Constants.java
@@ -3,16 +3,7 @@ package com.main.java.utils;
 public class Constants {
 
     public static final String DELIMITER = "\t";
-    public static final String AEROSPIKE_PROD_CONFIG_BUCKET = "reco.bd.aerospike.properties.prod";
-    public static final String AEROSPIKE_PREPROD_CONFIG_BUCKET = "reco.bd.aerospike.properties.preprod";
-    public static final String AEROSPIKE_MIRROR_CONFIG_BUCKET = "reco.bd.aerospike.properties.mirror";
-    public static final String AEROSPIKE_AUXILIARY_CONFIG_BUCKET = "reco.bd.aerospike.properties.auxiliary.prod";
-    public static final String AEROSPIKE_PRODUCTINFO_CONFIG_BUCKET = "reco.bd.aerospike.properties.productinfo.prod";
-    public static final String TOPOLOGY_CONFIG_BUCKET = "reco.bd.storm.properties.prod";
-    public static final String DATA_TYPE_TO_DATABASE_PARAMS_BUCKET_PROD = "reco.bd.data.types.db.params.prod";
-    public static final String DATA_TYPE_TO_DATABASE_PARAMS_BUCKET_PREPROD = "reco.bd.data.types.db.params.preprod";
-    public static final String DATA_TYPE_TO_DATABASE_PARAMS_BUCKET_DEBUG = "reco.bd.data.types.db.params.debug";
-    public static final String AEROSPIKE_SERVER_PREPROD_CONFIG_BUCKET = "reco.server.aerospike.properties.preprod";
-    public static final String AEROSPIKE_INFERENCE_CONFIG_BUCKET = "reco.raven.aerospike.inference.prod.v1";
+    public static final String TOPOLOGY_CONFIG_BUCKET = "storm.properties.prod";
+    public static final String DATA_TYPE_TO_DATABASE_PARAMS_BUCKET_PROD = "data.types.db.params.prod";
 }
 
