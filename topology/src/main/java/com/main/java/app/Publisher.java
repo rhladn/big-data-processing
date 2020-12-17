808cf6ee5cf264dbb31771b1de3bf0b1a59141dd
rahul.tandon <rahul.tandon@flipkart.com>
2020-12-17 14:55:12 +0530

    publisher

--- topology/src/main/java/com/main/java/app/Publisher.java
+++ topology/src/main/java/com/main/java/app/Publisher.java
@@ -8,6 +8,7 @@ import org.apache.storm.metric.LoggingMetricsConsumer;
 import org.apache.storm.topology.BoltDeclarer;
 import org.apache.storm.topology.IRichSpout;
 import org.apache.storm.topology.TopologyBuilder;
+import com.main.java.utils.DatabasePublisherBolt;
 import com.main.java.utils.StormUtilities;
 import com.google.common.collect.Sets;
 import org.apache.commons.lang.StringUtils;
@@ -27,29 +28,28 @@ public class Publisher {
     public static void main(String[] args) throws Exception {
 
         TopologyBuilder builder = new TopologyBuilder();
-        String inferenceASBuckets = "list of aerospike nodes";
-        // Initialize environment: prod or preprod
+        List<String> inferenceASBuckets = new ArrayList<String>(); // list of aerospike nodes seperated by com
+        // Initialize environment: prod
         String env = StringUtils.defaultIfEmpty(System.getenv(ENV), PROD_ENV);
-
 
         IRichSpout userCohortDataSpout = StormUtilities.getKafkaSpoutInstance(EMPLOYEEPROFILE.toString());
         builder.setSpout(EMPLOYEEPROFILE.toString()+ "Queue", userCohortDataSpout,
-                (int)ConfigClientUtils.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, SPOUT_PARALLELISM_CONFIG_KEY));
+                (int)FileClient.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, SPOUT_PARALLELISM_CONFIG_KEY));
         builder.setBolt(EMPLOYEEPROFILE.toString() + "DataPublisher", new DatabasePublisherBolt(AS_DB_TYPE,
                         inferenceASBuckets, DATA_TYPE_TO_DATABASE_PARAMS_BUCKET_PROD),
-                (int)ConfigClientUtils.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "inferenceBoltParallelism"))
+                (int)FileClient.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "inferenceBoltParallelism"))
                 .shuffleGrouping(EMPLOYEEPROFILE.toString() + "Queue");
 
         StormTopology topology = builder.createTopology();
 
 
         Config conf = new Config();
-        conf.setDebug(ConfigClientUtils.fetchConfigWithDefaultFallback(TOPOLOGY_CONFIG_BUCKET, "isDebug", true));
-        conf.setMessageTimeoutSecs((int) ConfigClientUtils.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "messageTimeout"));
-        conf.setNumWorkers((int) ConfigClientUtils.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "numWorkers"));
-        conf.setNumAckers((int) ConfigClientUtils.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "numAckers"));
-        conf.setMaxSpoutPending((int) ConfigClientUtils.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "maxSpoutPending"));
+        conf.setDebug(FileClient.fetchConfigWithDefaultFallback(TOPOLOGY_CONFIG_BUCKET, "isDebug", true));
+        conf.setMessageTimeoutSecs((int) FileClient.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "messageTimeout"));
+        conf.setNumWorkers((int) FileClient.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "numWorkers"));
+        conf.setNumAckers((int) FileClient.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "numAckers"));
+        conf.setMaxSpoutPending((int) FileClient.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "maxSpoutPending"));
         conf.registerMetricsConsumer(LoggingMetricsConsumer.class, 10);
-        StormSubmitter.submitTopologyWithProgressBar(ConfigClientUtils.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "topologyName") + "-" + env , conf, topology);
+        StormSubmitter.submitTopologyWithProgressBar(FileClient.fetchConfigCompulsary(TOPOLOGY_CONFIG_BUCKET, "topologyName") + "-" + env , conf, topology);
     }
 }
---
+++ topology/src/main/java/com/main/java/utils/DataPublisherBolt.java
@@ -0,0 +1,132 @@
+package com.main.java.utils;
+
+import com.codahale.metrics.JmxReporter;
+import com.codahale.metrics.MetricRegistry;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.storm.shade.org.apache.zookeeper.server.ExitCode;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Tuple;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static com.main.java.utils.Constants.DELIMITER;
+
+/**
+ * Bolt receives {@link Tuple}{@code (type, key, value)}. Based on {@code type}, it selects the
+ * appropriate params to write the {@code key-value} pair to the database.
+ */
+@Slf4j
+public class DatabasePublisherBolt extends BaseRichBolt {
+
+    private OutputCollector collector;
+    /**
+     * Holds params corresponding to various types of data.
+     */
+    private Map<String, IDbParams> semanticTypeToDbParams;
+    private List<IDatabase<String>> databases;
+    private MetricRegistry metricRegistry;
+    private JmxReporter reporter;
+    private String dbType;
+    private List<String> configBuckets;
+    private String dataTypeToDBParamBucket;
+
+    public DatabasePublisherBolt(String dbType, List<String> configBuckets, String dataTypeToDBParamBucket) {
+        this.dbType = dbType;
+        this.configBuckets = configBuckets;
+        this.dataTypeToDBParamBucket = dataTypeToDBParamBucket;
+    }
+
+    /**
+     * Initializes the params for the various types of data according to
+     * configs.
+     */
+
+    private void init() {
+        initSemanticTypeToDbParams();
+        FileClient.addListener(dataTypeToDBParamBucket, new BaseBucketUpdateListener(){
+            @Override
+            public void updated(Bucket oldBucket, Bucket newBucket) {
+                initSemanticTypeToDbParams();
+            }
+        });
+        databases = new ArrayList<>();
+        for (String configBucket: configBuckets){
+            IDatabase<String> database = DatabaseFactory.getDatabaseImpl(dbType, configBucket);
+            databases.add(database);
+        }
+        metricRegistry = new MetricRegistry();
+        reporter = JmxReporter.forRegistry(metricRegistry).build();
+        reporter.start();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initSemanticTypeToDbParams() {
+        semanticTypeToDbParams = FileClient.fetchDbParamsConfigFromGivenBucket(dataTypeToDBParamBucket);
+    }
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        this.collector = collector;
+        init();
+    }
+
+    /**
+     * Extracts {@code type, key, value} from {@link Tuple} and writes data to appropriate database
+     * based on type.
+     */
+    @Override
+    public void execute(Tuple tuple) {
+        String[] fields = tuple.getString(4).split(DELIMITER);
+        String type = fields[0];
+        String key = fields[1];
+        String value = fields[2];
+        IDbParams params = semanticTypeToDbParams.get(type);
+
+        if (params != null) {
+            Boolean success = true;
+            for(IDatabase<String> database: databases) {
+                ExitCode exitCode = database.put(key, value, params);
+                if (exitCode.equals(ExitCode.FAILURE)) {
+                    success = false;
+                }
+            }
+            if (success) {
+                collector.ack(tuple);
+                metricRegistry.meter("storm.ack").mark();
+            } else {
+                collector.fail(tuple);
+                metricRegistry.meter("storm.fail").mark();
+            }
+        } else {
+            log.error("No valid Db params found for " + type);
+            metricRegistry.meter("storm.invalidparams").mark();
+            collector.ack(tuple);
+        }
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+    }
+
+    /**
+     * Calls for each database.
+     */
+    @Override
+    public void cleanup() {
+        for(IDatabase<String> database: databases) {
+            try {
+                database.close();
+            } catch (IOException e) {
+                log.error("Unable to close connection to database", e);
+            }
+        }
+        reporter.stop();
+    }
+}
