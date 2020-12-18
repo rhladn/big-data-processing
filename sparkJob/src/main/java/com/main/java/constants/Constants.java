d4b54fbdaad677c9d135de2ff77a5893ad3c04ac
rahul.tandon <rahul.tandon@flipkart.com>
2020-12-18 17:12:11 +0530

    big-data

--- sparkJob/src/main/java/com/main/java/constants/Constants.java
+++ sparkJob/src/main/java/com/main/java/constants/Constants.java
@@ -1,9 +1,9 @@
 package com.main.java.constants;
 
 public class Constants {
-    public static String xClientIdKey = "X_CLIENT_ID";
-    public static String xClientIdValue = "m3aud.consumer.reco";
-    public static String url = "http://10.47.6.194/v0/segment/outputMeta/";
+    public static String xClientIdKey = "";
+    public static String xClientIdValue = "";
+    public static String url = "";
     public static String HASH_DELIMITER = "#";
     public static String DELIMITOR = "_";
 }
