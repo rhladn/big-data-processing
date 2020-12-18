d0e41e24ecf0edb20aa5f886db6cd8334bbe47f9
rahul.tandon <rahul.tandon@flipkart.com>
2020-12-18 15:18:55 +0530

    big-data

--- sparkJob/src/main/java/com/main/java/app/KafkaPublisher.java
+++ sparkJob/src/main/java/com/main/java/app/KafkaPublisher.java
@@ -2,7 +2,6 @@ package com.main.java.app;
 
 import com.main.java.avro.EmployeeProfile;
 import com.main.java.counters.Counters;
-import com.main.java.utils.PushToKafka;
 import com.main.java.utils.SparkAvroUtils;
 import org.apache.avro.Schema;
 import org.apache.spark.SparkConf;
@@ -17,13 +16,13 @@ import java.util.Map;
 public class KafkaPublisher {
 
     /**
-     * @param userCohortRDD rdd consisting of UserCohort data which is pushed to kafka key -> userId value -> EntityRelevance
+     * @param empProfileRDD rdd consisting of UserCohort data which is pushed to kafka key -> empId value -> EntityRelevance
      *                      EntityRelevance consists of a list of inference where each inference signifies a cohort
      * @param countersMap   The function is used to push data to kafka
      */
-    private static void publishUCtoKafka(JavaPairRDD<String, EmployeeProfile> userCohortRDD, Map<String, LongAccumulator> countersMap) {
-        userCohortRDD.foreach(tuple -> {
-            if (PushToKafka.getInstance().push(tuple._1, getEntityRelevance(tuple._2))) {
+    private static void publishUCtoKafka(JavaPairRDD<String, EmployeeProfile> empProfileRDD, Map<String, LongAccumulator> countersMap) {
+        empProfileRDD.foreach(tuple -> {
+            if (PushToKafka.getInstance().push(tuple._1, tuple._2)) { // function to push to Kafka with e
                 countersMap.get(Counters.KAFKA_SUCCESS).add(1L);
             } else {
                 countersMap.get(Counters.KAFKA_FAILURES).add(1L);
@@ -42,13 +41,13 @@ public class KafkaPublisher {
                 + "org.apache.hadoop.io.compress.BZip2Codec");
         Map<String, LongAccumulator> countersMap = Counters.createKafkaCounters(javaSparkContext);
         SparkAvroUtils.setAvroInputKeyValue(javaSparkContext, Schema.create(Schema.Type.STRING).toString(), EmployeeProfile.getClassSchema().toString());
-        JavaPairRDD<String, EmployeeProfile> userCohortRDD = SparkAvroUtils.readAsKeyValueInputeAvro(javaSparkContext, inputFilePath)
+        JavaPairRDD<String, EmployeeProfile> empProfileRDD = SparkAvroUtils.readAsKeyValueInputeAvro(javaSparkContext, inputFilePath)
                 .mapToPair(tuple->{
-                    String userId = (String) tuple._1.datum();
-                    EmployeeProfile userCohort = EmployeeProfile.newBuilder((EmployeeProfile) tuple._2.datum()).build();
-                    return new Tuple2<>(userId, userCohort);
+                    String empId = (String) tuple._1.datum();
+                    EmployeeProfile empProfile = EmployeeProfile.newBuilder((EmployeeProfile) tuple._2.datum()).build();
+                    return new Tuple2<>(empId, empProfile);
                 });
-        publishUCtoKafka(userCohortRDD, countersMap);
+        publishUCtoKafka(empProfileRDD, countersMap);
         javaSparkContext.stop();
     }
 }
--- sparkJob/src/main/java/com/main/java/constants/Constants.java
+++ sparkJob/src/main/java/com/main/java/constants/Constants.java
@@ -6,5 +6,4 @@ public class Constants {
     public static String url = "http://10.47.6.194/v0/segment/outputMeta/";
     public static String HASH_DELIMITER = "#";
     public static String DELIMITOR = "_";
-    public static String
 }
